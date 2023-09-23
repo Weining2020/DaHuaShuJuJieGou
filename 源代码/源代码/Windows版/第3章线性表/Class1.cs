2e488748d6a752d88bc2d222552af99d4a217284
Li Lei <lzlzlz2020@outlook.com>
2023-09-22 22:53:33 -0400

    20230922

---
+++ 源代码/源代码/Windows版/第3章线性表/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public List<int> InsertIntoList(int n, List<int> list, int value)
+		{
+			int listSize = list.Count;
+			if (n > listSize)
+			{
+				return WrongCode;
+			}
+
+			if (listSize == list.Capacity)
+			{
+
+				list.AddCapacity(1);
+			}
+
+			for (int i = listSize; i > n; i--)
+			{
+				list[i + 1] = list[i];
+			}
+
+			list[n] = value;
+
+			return list;
+
+		}
+	}
+}
